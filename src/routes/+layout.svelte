665f9a2e323e4cd5cca0ad8611a26795d370755d
User Verifried <120560136+userverifried@users.noreply.github.com>
2023-02-12 20:39:43 +1100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import "virtual:windi.css";
+  import "../app.css";
+  import { browser } from "$app/environment";
+  import Header from "$components/Header.svelte";
+  import Footer from "$components/Footer.svelte";
+
+  if (browser) {
+    import("virtual:windi-devtools");
+  }
+</script>
+
+<Header/>
+<main class="flex flex-col justify-center pt-10 px-4 min-h-121">
+  <slot />
+</main>
+<Footer/>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { AccordionItem } from "flowbite-svelte";
+
+</script>
+
+<h1>Heading 1</h1>
+
+<AccordionItem id="1">
+  <h2 slot="header">My Header 1</h2>
+  <div slot="body">
+    <p class="mb-2 text-gray-500 dark:text-gray-400">
+      Lorem ipsum dolor sit amet ...
+    </p>
+    ...
+  </div>
+</AccordionItem>
+<AccordionItem id="2">
+  <h2 slot="header">My Header 2</h2>
+  <div slot="body">
+    <p class="mb-2 text-gray-500 dark:text-gray-400">
+      Lorem ipsum dolor sit amet ...
+    </p>
+    ...
+  </div>
+</AccordionItem>
